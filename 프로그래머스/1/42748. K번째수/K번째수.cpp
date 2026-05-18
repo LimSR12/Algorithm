1d4190142c8de254a93d414ac77dda78e4f771c5
임승렬 <141974597+LimSR12@users.noreply.github.com>
2026-05-18 15:19:16 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for(auto command : commands) {
+        int i = command[0] - 1;
+        int j = command[1] - 1;
+        int k = command[2] - 1;
+        
+        vector<int> temp;
+        
+        for(int idx = i; idx <= j; idx++) {
+            temp.push_back(array[idx]);
+        }
+        
+        sort(temp.begin(), temp.end());
+        
+        answer.push_back(temp[k]);
+    }
+    
+    
+    
+    return answer;
+}
