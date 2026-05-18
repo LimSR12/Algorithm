f0da0e2be5d06b2c331800bfa0982f0acc97bcde
임승렬 <141974597+LimSR12@users.noreply.github.com>
2026-05-18 17:12:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.24 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,74 +1,43 @@
-#include<string>
-#include<vector>
-#include<climits>
-#include<queue>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int dn[] = {0, 1, 0, -1};
-int dm[] = {1, 0, -1, 0};
+int solution(vector<vector<int> > maps)
+{
+    int row = maps.size(); // 행 개수
+    int col = maps[0].size(); // 열 개수
 
-void DFS(vector<vector<int>> &maps, vector<vector<bool>> &visited, int n, int m, int dist, int &min_dist){
-    int row = maps.size();
-    int col = maps[0].size();
+    int dr[] = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+    int dc[] = {0, 0, -1, 1};
     
-    if(n == row - 1 && m == col - 1){
-        min_dist = min(dist, min_dist);
-        return;
-    }
-    
-    visited[n][m] = true;
-    
-    for(int i = 0; i < 4; i++){
-        int next_n = n + dn[i];
-        int next_m = m + dm[i];
-        if(next_n <= row - 1 && next_m <= col - 1 && next_n >= 0 && next_m >= 0 && 
-           maps[next_n][next_m] == 1 && !visited[next_n][next_m]){
-            DFS(maps, visited, next_n, next_m, dist + 1, min_dist);
-        }
-    }
-    
-    visited[n][m] = false;
-}
-
-int BFS(vector<vector<int>> &maps){
-    int row = maps.size();
-    int col = maps[0].size();
     vector<vector<bool>> visited(row, vector<bool>(col, false));
     vector<vector<int>> dist(row, vector<int>(col, 0));
     
-    queue<pair<int, int>> q;
-    q.push({0,0});
+    queue<pair<int,int>> q;
+    q.push({0, 0});
     visited[0][0] = true;
-    dist[0][0] = 1;
+    dist[0][0] = true;
     
-    while(!q.empty()){
-        auto [n, m] = q.front();
+    while(!q.empty()) {
+        auto [r, c] = q.front();
         q.pop();
         
-        for(int i = 0; i < 4; i++){
-            int next_n = n + dn[i];
-            int next_m = m + dm[i];
-            if(next_n <= row - 1 && next_m <= col - 1 && next_n >= 0 && next_m >= 0 && 
-                maps[next_n][next_m] == 1 && !visited[next_n][next_m]){
-                visited[next_n][next_m] = true;
-                dist[next_n][next_m] = dist[n][m] + 1;
-                q.push({next_n, next_m});
+        if(r == row - 1 && c == col - 1) {
+            return dist[r][c];
+        }
+        
+        for (int dir = 0; dir < 4; dir++) {
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
+            
+            if (nr >= 0 && nr < row && nc >= 0 && nc < col && maps[nr][nc] == 1 && !visited[nr][nc]) {
+                visited[nr][nc] = true;
+                dist[nr][nc] = dist[r][c] + 1;
+                q.push({nr, nc});
             }
         }
     }
     
-    if(dist[row - 1][col - 1] == 0){
-        return -1;
-    }else{
-        return dist[row - 1][col - 1];
-    }
+    return -1;
 }
 
-int solution(vector<vector<int> > maps)
-{
-    int answer = 0;
-    
-    answer = BFS(maps);
-    
-    return answer;
-}
