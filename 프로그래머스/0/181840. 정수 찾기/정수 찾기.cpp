c76b31c22cb80379f33ac5fd692a51a82a827e1a
임승렬 <141974597+LimSR12@users.noreply.github.com>
2026-05-18 15:28:38 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    int answer = 0;
+    
+    if (find(num_list.begin(), num_list.end(), n) != num_list.end()) {
+        answer = 1;
+    }
+    
+    return answer;
+}
